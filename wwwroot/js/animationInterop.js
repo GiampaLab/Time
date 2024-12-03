9485abacdddfb508e3521c04f8101d7a80533432
Giampaolo <giampaolo.paderno@justeattakeaway.com>
2024-12-03 14:13:00 +0000

    DElayed animation working

--- wwwroot/js/animationInterop.js
+++ wwwroot/js/animationInterop.js
@@ -1,13 +1,16 @@
 window.animationLoop = {
     requestAnimationFrame: function (dotNetHelper) {
        // console.log("Requesting animation frame...");
-        let frame = function () {
-            //console.log("Calling .NET method UpdateFrame");
-            dotNetHelper.invokeMethodAsync('UpdateFrame')
+        let frame = function (timeElapsed) {
+            dotNetHelper.invokeMethodAsync('UpdateFrame', timeElapsed)
                 .then(() => requestAnimationFrame(frame));
         };
 
         //console.log("Starting animation loop...");
         requestAnimationFrame(frame);
+    },
+    updateState: function(hourRef, minuteRef, hourState, minuteState){
+        hourRef.style.transform = "rotate("+hourState+"deg)"
+        minuteRef.style.transform = "rotate("+minuteState+"deg)"
     }
 };
