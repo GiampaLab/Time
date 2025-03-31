203ac57b0c9efb83868e8ebdbd4a9d8b6f8f0589
Giampaolo <giampaolo.paderno@justeattakeaway.com>
2025-03-31 17:22:57 +0100

    refactor

--- AnimationEngine/ChainedAnimationsManager.cs
+++ AnimationEngine/ChainedAnimationsManager.cs
@@ -4,21 +4,18 @@ namespace Time.AnimationEngine;
 
 public partial class ChainedAnimationsManager : IAnimationManager
 {
-    private readonly IList<IAnimationManager> animationManagers;
-    private readonly IList<int> animationDurations;
-    private IAnimationManager? animationManager;
+    private readonly IList<(IAnimationManager, int)> animationManagers;
+    private (IAnimationManager, int) animationManager;
     private bool animationManagerTimeIsUp = false;
-
     private int animationManagerIndex;
 
-    public ChainedAnimationsManager(IList<IAnimationManager> animationManagers, IList<int> animationDurations)
+    public ChainedAnimationsManager(IList<(IAnimationManager, int)> animationManagers)
     {
         this.animationManagers = animationManagers;
-        this.animationDurations = animationDurations;
         var myDotNetObjectReference = DotNetObjectReference.Create<IAnimationManager>(this);
         foreach (var animationManager in animationManagers)
         {
-            animationManager.SetDotNetObjectReference(myDotNetObjectReference);
+            animationManager.Item1.SetDotNetObjectReference(myDotNetObjectReference);
         }
     }
 
@@ -30,15 +27,15 @@ public partial class ChainedAnimationsManager : IAnimationManager
     [JSInvokable]
     public async void AnimationFinished()
     {
-        Console.WriteLine("Animation finished, Animation manager: " + animationManager?.GetAnimationType());
+        Console.WriteLine("Animation finished, Animation manager: " + animationManager.Item1.GetAnimationType());
         if (!animationManagerTimeIsUp)
         {
             // Delegate the call to the animation manager
-            animationManager?.AnimationFinished();
+            animationManager.Item1.AnimationFinished();
             return;
         }
         animationManagerTimeIsUp = false;
-        animationManager?.Stop();
+        animationManager.Item1.Stop();
         await InternalStart(animationManager);
     }
 
@@ -55,18 +52,18 @@ public partial class ChainedAnimationsManager : IAnimationManager
     {
     }
 
-    private IAnimationManager GetNextAnimationManager()
+    private (IAnimationManager, int) GetNextAnimationManager()
     {
         return animationManagers[animationManagerIndex++ % animationManagers.Count];
     }
 
-    private async Task InternalStart(IAnimationManager? previousAnimationManager)
+    private async Task InternalStart((IAnimationManager, int)? previousAnimationManager)
     {
         animationManager = GetNextAnimationManager();
-        animationManager.Start();
-        await Task.Delay(animationDurations[animationManagers.IndexOf(animationManager)]);
+        animationManager.Item1.Start();
+        await Task.Delay(animationManager.Item2);
         animationManagerTimeIsUp = true;
-        if (animationManager.GetAnimationType() == AnimationType.Infinite || animationManager.GetAnimationType() == AnimationType.Pattern)
+        if (animationManager.Item1.GetAnimationType() == AnimationType.Infinite || animationManager.Item1.GetAnimationType() == AnimationType.Pattern)
         {
             //needs to be triggered manually as the animation is infinite
             AnimationFinished();
